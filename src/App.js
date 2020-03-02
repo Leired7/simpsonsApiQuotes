91cfdf2e40159c4aec97a2cad7234c628cc37435
Leired7 <leirediez.7@gmail.com>
2020-03-02 12:25:57 +0100

    inicio proyecto reutilizando código de "simpsons-quotes-react"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar/Navbar';
+//import QuoteCard from './components/QuoteCard';
+import Quotelist from './components/Quotelist'
+import QuoteForm from './components/QuoteForm'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navbar />
+      <QuoteForm />
+      <Quotelist />
+        
     </div>
+
   );
-}
+};
 
 export default App;
