269f5626d07cddc9cc7e248a292782ccd3c04c48
Leired7 <leirediez.7@gmail.com>
2020-03-04 04:56:44 +0100

    simpsons quotes from API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
 import React from 'react';
+import './App.css'
+import axios from 'axios';
 import Navbar from './Navbar/Navbar';
-//import QuoteCard from './components/QuoteCard';
-import Quotelist from './components/Quotelist'
-import QuoteForm from './components/QuoteForm'
+import QuoteCard from './components/QuoteCard'
 
-function App() {
+const sampleSimpson = {
+  character: 'Marge Simpson',
+  characterDirection: 'Left',
+  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FMargeSimpson.png?1497567512205",
+  quote: 'Welcome to Simpsons Quote App-Web, click the button to refresh quote.',
+}
+
+class App extends React.Component {
+
+  constructor (props){
+    super(props);
+      this.state = {
+        character: sampleSimpson, 
+      }
+      this.getQuoteFromApi = this.getQuoteFromApi.bind(this);
+ }
+
+ getQuoteFromApi(){
+   const url = 'https://simpsons-quotes-api.herokuapp.com/quotes'
+   //Send request to api
+   axios.get(url)
+   //Extract DATA from the recived response
+   .then(response => response.data)
+   //Use this data to update the state
+   .then(simpsonsQuote => {
+     this.setState({
+       character: simpsonsQuote[0]
+     })
+     console.log('data',simpsonsQuote)
+  });
+ }
+
+ 
+
+render(){
+  
   return (
     <div className="App">
-      
       <Navbar />
-      <QuoteForm />
-      <Quotelist />
-        
+      <div className="quote-card">
+        <button className="refresh" type="button" onClick={this.getQuoteFromApi}>Show me another quote</button>
+        <QuoteCard character={this.state.character}/>
+      </div>
     </div>
-
   );
 };
+}
 
 export default App;
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,36 +1,21 @@
 import React from 'react';
 import './QuoteCard.css';
 
-class QuoteCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      favorite: false,
-    };
-  }
-  render() {
+function QuoteCard ({ character }) {
+
     return (
       <figure className="QuoteCard">
-        <img src={this.props.image} alt={this.props.character} />
+        <img src={character.image} alt={character.name}/>
         <figcaption>
-          <blockquote>{this.props.quote}</blockquote>
+          <blockquote>{ character.quote }</blockquote>
           <p>
-            <cite>{this.props.character}</cite>
-            <span
-              className={this.state.favorite ? 'is-favorite' : ''}
-              onClick={event => {
-                const newFavorite = !this.state.favorite;
-                this.setState({ favorite: newFavorite });
-              }}
-            >
-              &#9733;
-            </span>
+            <cite>{ character.character }</cite> 
           </p>
         </figcaption>
       </figure>
     );
-  }
 }
+
 
 export default QuoteCard;
 
